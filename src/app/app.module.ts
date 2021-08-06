6fcfcc4265987cdc155cbe4dc8886480a3fe84a3
mrraxa01 <54731146+mrraxa01@users.noreply.github.com>
2021-08-05 23:44:21 -0300

    Aula 1 - Primeiros passos para desenvolver com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { CourseListComponent } from "./courses/course-list.components";
+import { StarComponent } from "./star/star.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CourseListComponent, StarComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
